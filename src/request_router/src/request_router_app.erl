e15513182e760356faea6b3a0847c6d127041521
ed t <edwardt.tril@gmail.com>
2011-03-14 15:34:11 -0700

    some init code

--- src/request_router/src/request_router_app.erl
+++ src/request_router/src/request_router_app.erl
@@ -3,15 +3,37 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
+start()->
+    start
 
 start(_StartType, _StartArgs) ->
+    load_config(),
+    ensure_started([inets, sasl, os_mon, gproc]),
     request_router_sup:start_link().
 
 stop(_State) ->
+    cleanup().
     request_router_sup:terminate_child(),
     ok.
+
+
+load_config()->
+    app_util_config:read_config([], configuration_spec()).
+
+configuration_spec()->
+    [].
+    
+ensure_started([]) -> o;.
+ensure_started(Apps) when is_list(Apps)->
+    app_util:ensure_started(Apps).   
+
+cleanup()->
+    
+
+
+
